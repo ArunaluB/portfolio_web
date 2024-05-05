008b6e9d35e125b073ab175db306bc049d771127
ArunaluB <it22515612@my.sliit.lk>
2024-05-05 12:03:55 +0530

    about fix

---
+++ src/components/info/Info.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import { personalInfo } from '../../data';
+
+const Info = () => {
+    return (
+        <>
+            {personalInfo.map(({ title, description }, index) => {
+                return (
+                    <li className="info__item" key={index} >
+                        <span className="info__title" > {title}  </span>
+                        <span className='info__description'> {description}  </span>
+
+                    </li>
+
+                )
+
+            })}
+        </>
+
+
+    )
+};
+
+export default Info;
